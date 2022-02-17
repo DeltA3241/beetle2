4737460d2d2a7a88ca4c67bee5a293105109d26d
Baeyung <ahmadsheikh617@gmail.com>
2022-02-17 23:02:41 +0500

    --made initial changes to the app

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const AppBarTheme kAppBarTheme = AppBarTheme(
+  backgroundColor: Colors.white,
+  titleTextStyle: kAppBarTextStyle,
+  elevation: 10,
+  centerTitle: true,
+);
+
+const TextStyle kAppBarTextStyle = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.bold,
+  color: Colors.orange,
+);
